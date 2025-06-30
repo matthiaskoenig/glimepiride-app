bfdb79cee428b98223b436b69cd36420fa3d6b53
Matthias König <konigmatt@googlemail.com>
2025-06-30 14:31:34 +0200

    docker cleanup

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,12 @@
 git pull
+
+# shut down all containers (remove images and volumes)
+docker compose -f docker-compose.yml down --volumes --rmi local
+
+# make sure images are removed
+docker image rm -f glimepiride-app_backend:latest
+
+# cleanup all dangling images, containers, volumes and networks
+docker system prune --force
+
 docker-compose -f docker-compose.yml up --force-recreate --always-recreate-deps --build --detach
